dbe03acdec85008b25c0b26be45b674937a5e5c4
showlotus <2257030399@qq.com>
2025-03-19 14:17:03 +0800

    chore: update build config

--- demos/webpack5/webpack.config.js
+++ demos/webpack5/webpack.config.js
@@ -3,7 +3,6 @@ const CompilerThenPlugin = require('compiler-then-plugin')
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
